8982afd6434fc34944753c7e1b988b979fd6f2af
Anne <3dgame.umschulung@gmail.com>
2018-12-07 12:43:17 +0100

    Konzept und Workfiles hinzugefügt

---
+++ Unity/Pruefung/Assets/_Scripts/Gameplay/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	[SerializeField] public Transform player;
+	//[SerializeField] PlayerMovement playerMove;
+	public Vector2 camMove;
+
+	private void Update()
+	{
+		camMove.x = player.transform.position.x;
+
+		//if (Player.colCheck) { }
+		camMove.y = player.transform.position.y;
+	}
+
+
+}
---
+++ Unity/Pruefung/Assets/_Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Transform cam;
+	public float speed = 4f;
+	public float jumpSpeed = 7f;
+	public float gravity = 20f;
+	private Vector3 moveDirection = Vector3.zero;
+	private Vector3 cameraRot;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update()
+	{
+		Move();
+		//LockMouse();
+		//ReleaseMouse();
+	}
+
+	public void Move()
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+
+		// is the controller on the ground?
+		if (controller.isGrounded)
+		{
+			//Feed moveDirection with input.
+			
+			//moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+			
+			//Transform local values to world space
+			moveDirection = transform.TransformDirection(moveDirection);
+
+			//Multiply it by speed.
+			moveDirection *= speed;
+			//Jumping
+			if (Input.GetButton("Jump")) { moveDirection.y = jumpSpeed; }
+		}
+
+		//Applying gravity to the controller
+		moveDirection.y -= gravity * Time.deltaTime;
+		//Making the character move
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	//public void LockMouse()
+	//{
+	//	if (Input.GetKeyDown(KeyCode.L))
+	//	{ Cursor.lockState = CursorLockMode.Locked; }
+	//}
+
+	//public void ReleaseMouse()
+	//{
+	//	if (Input.GetKeyDown(KeyCode.U))
+	//	{ Cursor.lockState = CursorLockMode.None; }
+	//}
+}
